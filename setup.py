e80e2c385f62c6f27f3af1ccef05cfbc63fc0aab
vince <Vince-C156>
2023-06-09 13:31:57 -0700

    replaced setup

--- setup.py
+++ setup.py
@@ -1,131 +1,32 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+"""Installation script for the 'isaacgymenvs' python package."""
 
-# Note: To use the 'upload' functionality of this file, you must:
-#   $ pipenv install twine --dev
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
 
-import io
+from setuptools import setup, find_packages
+
 import os
-import sys
-from shutil import rmtree
 
-from setuptools import find_packages, setup, Command
 
-# Package meta-data.
-NAME = 'RLdocking'
-DESCRIPTION = 'Space docking with PPO using stable baselines 3'
-URL = 'https://github.com/Vince-C156/PLANNING-AUTONOMOUS-SPACECRAFT-RENDEZVOUS-AND-DOCKING-TRAJECTORIES-VIA-REINFORCEMENT-LEARNING'
-EMAIL = 'vincent.chencs@gmail.com.com'
-AUTHOR = 'Vincent Beau Chen'
-REQUIRES_PYTHON = '>=3.8'
-VERSION = '0.1.0'
-
-# What packages are required for this module to be executed?
-REQUIRED = [
-     'requests', 'maya', 'records',
+# Minimum dependencies required prior to installation
+INSTALL_REQUIRES = [
+    "torch==2.0.1",
+    "stable_baselines3==2.0.0a8",
+    "gymnasium==0.28.1",
+    "numpy==1.23.5"
 ]
 
-# What packages are optional?
-EXTRAS = {
-    'fancy feature': ['numba'],
-}
-
-# The rest you shouldn't have to touch too much :)
-# ------------------------------------------------
-# Except, perhaps the License and Trove Classifiers!
-# If you do change the License, remember to change the Trove Classifier for that!
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
-try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
-except FileNotFoundError:
-    long_description = DESCRIPTION
-
-# Load the package's __version__.py module as a dictionary.
-about = {}
-if not VERSION:
-    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
-    with open(os.path.join(here, project_slug, '__version__.py')) as f:
-        exec(f.read(), about)
-else:
-    about['__version__'] = VERSION
-
-
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-        self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/*')
-
-        self.status('Pushing git tags…')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
-
-        sys.exit()
-
-
-# Where the magic happens:
+# Installation operation
 setup(
-    name=NAME,
-    version=about['__version__'],
-    description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    #packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-    # If your package is a single module, use this instead of 'packages':
-    py_modules=['mypackage'],
-
-    entry_points={
-        'console_scripts': ['mycli=mymodule:cli'],
-    },
-    #install_requires=REQUIRED,
-    #extras_require=EXTRAS,
+    name="rlDocking",
+    author="Vincent Beau Chen",
+    version="2023.7.1.0",
+    description="RL environment for space docking with ppo",
+    keywords=["robotics", "rl"],
     include_package_data=True,
-    license='MIT',
-    classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
-    ],
-    # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-    },
+    install_requires=INSTALL_REQUIRES,
+    packages=find_packages("."),
+    classifiers=["Natural Language :: English", "Programming Language :: Python :: 3.8, 3.9, 3.10"],
+    zip_safe=False,
 )
